c4be750a3dcaff74fab4fd9f686eb9ea2ae97a30
AlexShadow <57513887+Alex22112000@users.noreply.github.com>
2024-02-05 15:50:51 +0700

    add IUserRepository.java

---
+++ backend/src/main/java/application/product/User.java
@@ -0,0 +1,32 @@
+package application.auth.user;
+
+public class User {
+    
+    private String login;
+    private String password;
+    private String role;
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
